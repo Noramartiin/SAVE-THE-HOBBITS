c84422f1260847d6ca85a970c787013992acd944
noramartiin <noramartiin96>
2021-01-28 19:48:17 +0100

    Clean Code

--- script.js
+++ script.js
@@ -1,25 +1,60 @@
 let canvas = document.querySelector("canvas");
 let ctx = canvas.getContext("2d");
-// cavas.height= 726;
-// canvas.width= 1125;
 canvas.style.border = "2px solid black";
 canvas.style.width = "553px";
 canvas.style.height = "369px";
+
+//DOM + VARIABLES
+
 let startBtn = document.querySelector("#start-btn");
-let restartBtn = document.querySelector("#restart-btn");
 let cleanEl = document.querySelector("#cleanEl");
+let endEl = document.querySelector("#endEl");
+let restartBtn = document.querySelector("#restart-btn");
 let scoreEl = document.querySelector("#scoreEl");
 let score = 0;
-let intervalID = 0;
-// scoreEl.innerHTML= score
-
-let endEl = document.querySelector("#endEl");
-
-let life = 3;
 let lifeEl = document.querySelector("#lifeEl");
-
+let life = 3;
 let numorc = 0;
 let numorcEl = document.querySelector("#numorcEl");
+let intervalID = 0;
+
+//RESTART-VARIABLES
+
+function init() {
+  score = 0;
+  life = 3;
+  numorc = 0;
+  let player = {
+    x: 100,
+    y: 100,
+    width: 16,
+    height: 24,
+    frameX: 0,
+    frameY: 0,
+    speed: 1,
+    moving: false,
+  };
+  quiver = [];
+  hobbits = [new Hobbit()];
+  enemies = [new Orc()];
+  arrow = new Arrow(player.x + 10, player.y + 10, 15, 1, 4, canvas.width);
+}
+
+//IMAGES
+
+const backImg = new Image();
+backImg.src = "./Images/Background.jpg";
+
+const playerSprite = new Image();
+playerSprite.src = "./Images/Legolas.png";
+
+let enemy = document.createElement("img");
+enemy.src = "./Images/Orc-left.png";
+
+let hobbit = document.createElement("img");
+hobbit.src = "./Images/Hobbit.png";
+
+//SOUNDS
 
 let startm = document.querySelector("#startm");
 startm.volume = 0.15;
@@ -48,40 +83,7 @@ orcWins.volume = 1;
 let hobbitDeath = document.querySelector("#hobbitDeath");
 hobbitDeath.volume = 1;
 
-function init() {
-  score = 0;
-  life = 3;
-  numorc = 0;
-  let player = {
-    x: 100,
-    y: 100,
-    width: 16,
-    height: 24,
-    frameX: 0,
-    frameY: 0,
-    speed: 1,
-    moving: false,
-  };
-  quiver = [];
-  hobbits = [new Hobbit()];
-  enemies = [new Orc()];
-  arrow = new Arrow(player.x + 10, player.y + 10, 15, 1, 4, canvas.width);
-}
-
-const backImg = new Image();
-backImg.src = "./Images/unnamed (1).jpg";
-
-const playerSprite = new Image();
-playerSprite.src = "./Images/bloodelf_male3 (1).png";
-
-let enemy = document.createElement("img");
-enemy.src = "./Images/Orc.izq.png";
-
-let hobbit = document.createElement("img");
-hobbit.src = "./Images/hobbit.rct.png";
-
-// let bloodImg = new Image();
-// bloodImg.src = "./images/blood.1.png";
+//PLAYER
 
 let keys = [];
 
@@ -131,6 +133,8 @@ function movePlayer() {
   }
 }
 
+//ARROW
+
 class Arrow {
   constructor(x, y, width, height, moving) {
     this.x = player.x + 10;
@@ -170,6 +174,7 @@ document.addEventListener("keydown", function (event) {
 });
 
 //ORC
+
 class Orc {
   constructor(x, y, width, height) {
     this.x = canvas.width;
@@ -188,15 +193,12 @@ function spawnEnemies() {
   for (i = 0; i < enemies.length; i++) {
     enemies[i].drawEnemy();
     orcWalk.play();
-    // sprawnBlood()
-    //    if (enemies[i].x == 170) {
-    //      enemies.push(new Orc(), );
-    //    }
     enemies[i].x--;
   }
 }
 
 //HOBBIT
+
 class Hobbit {
   constructor(x, y, width, height) {
     this.x = canvas.width;
@@ -221,39 +223,7 @@ function spawnHobbits() {
   }
 }
 
-//////////////////
-
-// function sprawnBlood(){
-//     ctx.drawImage(bloodImg, enemies.x, enemies.y, 20, 30);
-// }
-
-// class Blood {
-//   constructor(x, y, width, height) {
-//     this.x = enemies.x;
-//     this.y = enemies.y;
-//     this.width = 20;
-//     this.height = 30;
-//   }
-//   drawBlood() {
-//     ctx.drawImage(bloodImg, this.x, this.y, this.width, this.height);
-//   }
-// }
-
-// let blood = [new Blood()];
-
-// function spawnBlood() {
-//   for (i = 0; i < blood.length; i++) {
-//     blood[i].drawBlood();
-//     //    if (enemies[i].x == 170) {
-//     //      enemies.push(new Orc(), );
-//     //    }
-//     // enemies[i].x--;
-//   }
-// }
-
-//////////////////
-
-//////////////////
+//GAME
 
 function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH) {
   ctx.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH);
@@ -278,6 +248,8 @@ function animate() {
   throwArrow();
   spawnHobbits();
 
+  //COLLISION-1
+
   for (let j = 0; j < enemies.length; j++) {
     for (let i = 0; i < quiver.length; i++) {
       if (
@@ -289,14 +261,8 @@ function animate() {
         orcWalk.pause();
         orcDeath.play();
         enemies.splice(j, 1);
-        // enemies[j].push(new Blood())
-
         quiver.splice(i, 1);
-        // enemies[i].x = -20
-        //console.log('colission')
         enemies.push(new Orc());
-
-        // blood.push(new Blood());
         score += 10;
         scoreEl.innerHTML = score;
         numorc += 1;
@@ -304,6 +270,8 @@ function animate() {
       }
     }
   }
+
+  //COLLISION-2
 
   for (let j = 0; j < hobbits.length; j++) {
     for (let i = 0; i < quiver.length; i++) {
@@ -316,16 +284,16 @@ function animate() {
         hobbitDeath.play();
         hobbits.splice(j, 1);
         quiver.splice(i, 1);
-        // enemies[i].x = -20
-        //console.log('colission')
         hobbits.push(new Hobbit());
-        // score -=5
-        // scoreEl.innerHTML= score
+        score -= 5;
+        scoreEl.innerHTML = score;
         life -= 1;
         lifeEl.innerHTML = life;
       }
     }
   }
+
+  //PLAYER-DIES
 
   if (life <= 0) {
     orcWalk.pause();
@@ -336,6 +304,26 @@ function animate() {
     gameOver();
   }
 }
+
+//CHANGE-STATES
+
+function startGame() {
+  cleanEl.style.display = "none";
+  endEl.style.display = "none";
+  canvas.style.display = "block";
+  gamem.play();
+
+  intervalID = setInterval(() => {
+    requestAnimationFrame(animate);
+  }, 15);
+}
+
+startBtn.addEventListener("click", function () {
+  startm.pause();
+  swordsm.play();
+  swordsm.loop = false;
+  startGame();
+});
 
 function gameOver() {
   canvas.style.display = "none";
@@ -354,27 +342,9 @@ restartBtn.addEventListener("click", () => {
   orcWins.pause();
 });
 
-function startGame() {
-  cleanEl.style.display = "none";
-  endEl.style.display = "none";
-  canvas.style.display = "block";
-  gamem.play();
-
-  intervalID = setInterval(() => {
-    requestAnimationFrame(animate);
-  }, 15);
-}
-
 window.addEventListener("load", () => {
   startm.play();
   canvas.style.display = "none";
   cleanEl.style.display = "block";
   endEl.style.display = "none";
-});
-
-startBtn.addEventListener("click", function () {
-  startm.pause();
-  swordsm.play();
-  swordsm.loop = false;
-  startGame();
 });
